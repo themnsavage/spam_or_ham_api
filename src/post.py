7d055c8d90d695f5be12cc930bb62fe91ed0e4fc
themnsavage <mnsavage@crimson.ua.edu>
2023-11-23 15:33:47 -0600

    feat: add layer for model_manager

--- src/post.py
+++ src/post.py
@@ -1,4 +1,12 @@
 import json
+import os
+import sys
+
+if "AWS_EXECUTION_ENV" not in os.environ:
+    from ...sms_messages_as_spam_or_ham.src.model_manager import Model_Manager
+else:
+    sys.path.append("/opt/")
+    from src.model_manager import Model_Manager
 
 def lambda_handler(event, context):
     
